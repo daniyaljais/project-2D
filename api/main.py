b51ac170e600d2ef9db6b990479d6f0231fb59f3
daniyaljais <21f3000031@ds.study.iitm.ac.in>
2025-03-30 23:32:47 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI, Form, File, UploadFile  # type: ignore
 from fastapi.responses import HTMLResponse  # type: ignore
 from fastapi.middleware.cors import CORSMiddleware  # type: ignore
